f219d6943b6fbc6c3dc63e80320de094a8f35706
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-10-12 17:28:43 +0530

    itemcart done

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,7 +5,7 @@ import ItemCard from "./ItemCard";
 const Cart = () => {
   return (
     <>
-      <div className="fixed right-0 top-0 w-full lg:w-[20vw] h-full p-5 bg-white mb-3">
+      <div className="fixed right-0 top-0 w-full lg:w-[20vw] h-full p-5 bg-white mb-3 ">
         <div className="flex justify-between items-center my-3">
           <span className="text-xl font-bold text-gray-800">My Order</span>
           <IoMdClose className="border-2 border-gray-600 text-gray-600 font-bold  p-1 text-xl  rounded-md hover:text-red-300 hover:border-red-300 cursor-pointer" />
