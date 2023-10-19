82d2f13c41e022c2ec3a577f62b1c00151cfd983
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-10-19 14:23:13 +0530

    redux toolkit done

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-const Store = configureStore({})
+import CartSlice from "./slices/CartSlice";
+import CategorySlice from "./slices/CategorySlice"
+import SearchSlice from "./slices/SearchSlice"
+const Store = configureStore({
+    reducers: {
+        cart: CartSlice,
+        category: CategorySlice,
+        search: SearchSlice,
+    },
+});
 export default Store;
