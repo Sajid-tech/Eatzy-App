e8d96a84e314c17ace89f1050bd44cc41ad3288e
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-10-22 16:57:59 +0530

    navbar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,23 +5,25 @@ import { setSearch } from "../redux/slices/SearchSlice";
 const Navbar = () => {
   const dispatch = useDispatch();
   return (
-    <nav className="flex flex-col lg:flex-row justify-between py-3 mx-6 mb-10">
-      <div>
-        <h3 className="text-xl font-bold text-gray-600">
-          {new Date().toUTCString().slice(0, 16)}
-        </h3>
-        <h1 className="text-2xl font-bold ">EATZY FOODS</h1>
-      </div>
-      <div>
-        <input
-          type="search"
-          name="search"
-          id=""
-          placeholder="Search here"
-          autoComplete="off"
-          onChange={(e) => dispatch(setSearch(e.target.value))}
-          className="p-3 border border-gray-400 text-sm rounded-lg outline-none w-full lg:w-[25vw]"
-        />
+    <nav className="relative px-2 pt-2 md:px-4 md:pt-4 z-10">
+      <div className="relative flex justify-between py-4 h-16 px-4 lg:px-8 text-white bg-slate-900 dark:bg-slate-800 dark:border dark:border-slate-700 dark:shadow-xl gap-6 lg:gap-8 rounded-md shadow-md">
+        <div>
+          {/* <h3 className="text-xl font-bold text-gray-600">
+            {new Date().toUTCString().slice(0, 16)}
+          </h3> */}
+          <h1 className="text-xl font-bold ">EATZY FOODS</h1>
+        </div>
+        <div>
+          <input
+            type="search"
+            name="search"
+            id=""
+            placeholder="SEARCH..."
+            autoComplete="off"
+            onChange={(e) => dispatch(setSearch(e.target.value))}
+            className="md:w-64 w-32 px-3 py-1 border-2 border-indigo-400 rounded-md text-indigo-400 hover:text-indigo-300 hover:border-indigo-300 uppercase font-bold modal-trigger"
+          />
+        </div>
       </div>
     </nav>
   );
