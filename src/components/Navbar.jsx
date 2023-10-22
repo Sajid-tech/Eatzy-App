7416516bfe51a05d2319844cb3711487612c418a
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-10-22 20:26:57 +0530

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,6 @@ const Navbar = () => {
     <nav className="relative px-2 pt-2 md:px-4 md:pt-4 z-10">
       <div className="relative flex justify-between py-4 h-16 px-4 lg:px-8 text-white bg-slate-900 dark:bg-slate-800 dark:border dark:border-slate-700 dark:shadow-xl gap-6 lg:gap-8 rounded-md shadow-md">
         <div>
-          {/* <h3 className="text-xl font-bold text-gray-600">
-            {new Date().toUTCString().slice(0, 16)}
-          </h3> */}
           <h1 className="text-xl font-bold ">EATZY FOODS</h1>
         </div>
         <div>
